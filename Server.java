0446d2f29d3c2c1c08e4833e32b263c706ce99fa
Thembiso Ragimana <41780221+Thembiso@users.noreply.github.com>
2019-05-14 15:43:10 +0200

    Add files via upload

--- Server.java
+++ Server.java
@@ -1,50 +1,56 @@
-package clientServer;
 import java.net.*;
-import java.security.Key;
-import java.security.NoSuchAlgorithmException;
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.*;
 import java.security.spec.InvalidKeySpecException;
-//import java.util.*;
 import java.io.*;
+import java.util.*;
+
 
 public class Server {
+
     ServerSocket ss;
     Socket s;
-    Key pub;
-    Key priv;
+    Key publicKey;
+    Key privateKey;
     PrintWriter p;
     GenerateRSAKeys keygen;	//object to create and save RSA keys
+    private static Cipher cipher = null;
 
-    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+
+    public Server () throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
+    
+      this.CreateKeys();	// Create private and public keys
+      System.out.println("Public-Private key pair created for server");
+      System.out.println("Server Public key saved to file PubkeyServer.txt");
+      
+    }
+    
+    
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, Exception, InvalidKeySpecException {
         Server server = new Server();
         server.run();
     }
 
-    public void run() throws NoSuchAlgorithmException, InvalidKeySpecException{
+    public void run() throws NoSuchAlgorithmException, Exception,  InvalidKeySpecException{
         
         try{
             ss = new ServerSocket(4999);
             
                 System.out.println("Waiting for client");
                 s = ss.accept();	// Accept client connection
-
                 System.out.println("Client connected");	
                 
-                this.CreateKeys();	// Create private and public keys
-                System.out.println("Public-Private key pair created for server");
-               // keygen.getKeySpec("Pubkey.txt",this.pub);	// saves public key to a file called pubkey
-                System.out.println("Server Public key saved to file PubkeyServer.txt");
-                
-                
                 while(s.isConnected()){
-                BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            
-                String str = br.readLine();
-                System.out.println("Message + encrypted hash from client: " + str);
-
-                if(str.equals("")){
-                    break;
-                }
-            }      
+                
+                  BufferedInputStream bs = new BufferedInputStream(s.getInputStream());
+                  byte[] byteArray = new byte[1024];
+                  int byt = bs.read(byteArray);
+                
+                  byte [] messageFromGareth = decryptAndUnzip ( byteArray, byt ); // this returns a byte [] from gareth
+                   
+                   
+                  }      
                 
         }
         catch(IOException e){
@@ -52,16 +58,59 @@ public class Server {
         }
     }
     
-    // Keys are created and then saved onto the server
-    public void CreateKeys() throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
-    	this.keygen = new GenerateRSAKeys(2);	// creates a public and a private key
-    	
-    	Key[] keyring = new Key[2];			// position 0 has public key, position 1 has private key
-    	keyring = keygen.KeyPairGen(2048);
-    	this.pub = keyring[0];
-        this.keygen.getKeySpec("PubkeyServer.txt",this.pub);	// saves public key to a file called pubkey
-    	this.priv = keyring[1];
+    /**
+    *
+    * decrypt takes in a byte array with message structure [ key_session + messasge ] with key 
+    * encrypted with the public key and message encrypted with session_key
+    * Uses RSA with private key to decrypt the key and AES to decrypt the message 
+    *   
+    **/
+    public byte [] decryptAndUnzip(byte [] byteArray, int len) throws Exception{
+      
+            //deccrypt the Ks with public(pub) key
+            byte [] byteArrayKey = Arrays.copyOfRange(byteArray, 0,32); //extract key from recieved data
+            System.out.println("Key data: >>" + new String(byteArrayKey, "UTF8") );
+            
+            //decrypt with private key
+            cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.DECRYPT_MODE, privateKey); 
+            SecretKey secretKey = new SecretKeySpec(byteArrayKey, 0, byteArrayKey.length, "AES");
+            System.out.println("Key generated : " + Base64.getEncoder().encodeToString(secretKey.getEncoded()) );
+            
+            //use Ks to decrypt the message
+            byte [] byteArrayData = Arrays.copyOfRange(byteArray, 32,len); //extract message
+            System.out.println("Encrypted message: " + new String(byteArrayData, "UTF8") );
+             
+            //decrypt message with AES 
+            cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+      		byte[] decryptedBytes = cipher.doFinal(byteArrayData);
+          
+           //unziip and print message
+            System.out.println( "decrypted message is >>" + new String(decryptedBytes, "UTF8") );
+            
+            return decryptedBytes;
     }
+
+    
+    /** 
+    * CreateKeys() create public and private key 
+    * for the server, Keys are created and then saved onto the server (txt file _ pubkeyServer.txt )
+    *
+    **/
+    public void CreateKeys() throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
+    	
+      this.keygen = new GenerateRSAKeys(2);	
+    	Key[] keyring = new Key[2];			                           // position 0 has public key, position 1 has private key
+    	keyring = keygen.KeyPairGen(2048);
+      
+    	this.publicKey = keyring[0];
+      this.privateKey = keyring[1];
+      
+      this.keygen.getKeySpec("PubkeyServer.txt",this.publicKey);    	// saves public key to a file called pubkey
+    }
+    
+    
     
 }
 
