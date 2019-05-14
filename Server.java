c1b51bb2f018051e09a722b3abcf964f5dea6d51
Thembiso Ragimana <41780221+Thembiso@users.noreply.github.com>
2019-05-14 16:45:16 +0200

    Add files via upload

--- Server.java
+++ Server.java
@@ -14,8 +14,11 @@ public class Server {
     Key publicKey;
     Key privateKey;
     PrintWriter p;
-    GenerateRSAKeys keygen;	//object to create and save RSA keys
+    GenerateRSAKeys keygen;
     private static Cipher cipher = null;
+    private final static String ALGORITHM_AES = "AES/CBC/PKCS5Padding";
+    private final static String ALGORITHM_RSA = "RSA/ECB/PKCS1Padding";
+
 
 
     public Server () throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
@@ -47,10 +50,23 @@ public class Server {
                   byte[] byteArray = new byte[1024];
                   int byt = bs.read(byteArray);
                 
-                  byte [] messageFromGareth = decryptAndUnzip ( byteArray, byt ); // this returns a byte [] from gareth
-                   
-                   
-                  }      
+                  String messageAndHash = new String( decryptAndUnzip ( byteArray, byt ), "UTF8"); // this returns a byte [] from gareth
+                  
+                  System.out.println(" Decrypting Message and Hash...  " +  messageAndHash);
+                  //check if message hash and match
+                  String hash = decrypt ( GenerateRSAKeys.readKeyFromFile("PubkeyClient.txt"), messageAndHash.substring(messageAndHash.indexOf('+')+1).getBytes() , 0).toString();
+                  String message = messageAndHash.substring(0, messageAndHash.indexOf('+'));
+                  
+                  if ( hash.equals(sha1(message))){
+                     System.out.println(" Decrypted Message hash and Hash are EQUAL  ");
+                     System.out.println(sha1(message) +" and " + hash);
+
+                  }
+                  else {
+                     System.out.println(" Decrypted Message hash and Hash are NOT EQUAL  ");
+                     System.out.println(sha1(message) +" and " + hash);
+                  } 
+             }      
                 
         }
         catch(IOException e){
@@ -62,36 +78,73 @@ public class Server {
     *
     * decrypt takes in a byte array with message structure [ key_session + messasge ] with key 
     * encrypted with the public key and message encrypted with session_key
-    * Uses RSA with private key to decrypt the key and AES to decrypt the message 
-    *   
+    * Uses RSA with private key to decrypt the key and AES to decrypt the message
+    *
+    * @return byte [] decryptedBytes
+    * 
+    * @throws NoSuchAlgorithmException
+    * @throws InvalidAlgorithmParameterException
+    * @throws InvalidKeyException   
     **/
-    public byte [] decryptAndUnzip(byte [] byteArray, int len) throws Exception{
+    public byte [] decryptAndUnzip (byte [] byteArray, int len) throws Exception{
       
             //deccrypt the Ks with public(pub) key
-            byte [] byteArrayKey = Arrays.copyOfRange(byteArray, 0,32); //extract key from recieved data
-            System.out.println("Key data: >>" + new String(byteArrayKey, "UTF8") );
-            
-            //decrypt with private key
-            cipher = Cipher.getInstance("RSA");
-            cipher.init(Cipher.DECRYPT_MODE, privateKey); 
-            SecretKey secretKey = new SecretKeySpec(byteArrayKey, 0, byteArrayKey.length, "AES");
-            System.out.println("Key generated : " + Base64.getEncoder().encodeToString(secretKey.getEncoded()) );
+            byte [] byteArrayKey = Arrays.copyOfRange(byteArray, 0,32);                                                 //extract key from recieved data
+            System.out.println("\n Key data: >>" + new String(byteArrayKey, "UTF8") );
+            byteArrayKey = decrypt ( privateKey , byteArrayKey , 0 ) ;                                                   //decrypt key with RSA
+            SecretKey secretKey = new SecretKeySpec(byteArrayKey, 0, byteArrayKey.length, "AES");                        //generate key from the data
+            System.out.println(" Key generated : " + Base64.getEncoder().encodeToString(secretKey.getEncoded()) );
             
             //use Ks to decrypt the message
             byte [] byteArrayData = Arrays.copyOfRange(byteArray, 32,len); //extract message
-            System.out.println("Encrypted message: " + new String(byteArrayData, "UTF8") );
+            System.out.println(" Encrypted message:\n>> " + new String(byteArrayData, "UTF8") );
              
             //decrypt message with AES 
-            cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
-      		byte[] decryptedBytes = cipher.doFinal(byteArrayData);
+            byte[] decryptedBytes = decrypt ( secretKey , byteArrayKey , 1 ) ;                                            //decrypt with AES
           
-           //unziip and print message
-            System.out.println( "decrypted message is >>" + new String(decryptedBytes, "UTF8") );
-            
+            //unziip and print message
+           
+            System.out.println( " decrypted message is:\n >>" + new String(decryptedBytes, "UTF8") );
             return decryptedBytes;
     }
+    
+    // This method reduces the message to a single hashed value for digital signature
+    static String sha1(String input) throws NoSuchAlgorithmException {
+    
+        MessageDigest mDigest = MessageDigest.getInstance("SHA1");
+        byte[] result = mDigest.digest(input.getBytes());
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < result.length; i++) {
+            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+        }
 
+        return sb.toString();
+    }
+    
+    /**
+     * Method decrypt messages with the Key and specified algorithm
+     * 0 for RSA and 1 for AES.
+     *
+     * @param publicKey uses generated public key.
+     * @param message
+     * @return
+     * @throws NoSuchPaddingException
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeyException
+     * @throws BadPaddingException
+     * @throws IllegalBlockSizeException
+     */
+     
+    public byte [] decrypt (Key key, byte [] message , int n) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, IOException {
+
+        if (n==0)
+        this.cipher = Cipher.getInstance(ALGORITHM_RSA);
+        else
+        this.cipher = Cipher.getInstance(ALGORITHM_AES);
+        
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return  cipher.doFinal(message);
+    }
     
     /** 
     * CreateKeys() create public and private key 
